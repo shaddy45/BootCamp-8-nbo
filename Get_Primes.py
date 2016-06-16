9cc3ec718cccd4e071e26f9a89edd4a65e4e53b7
Cyrus Christopher <christophercyrus2012@gmail.com>
2016-06-16 12:28:27 +0300

    Ammended code to ask for input from user

--- Get_Primes.py
+++ Get_Primes.py
@@ -1,20 +1,30 @@
 '''
-Code to print the nth prime number 
+Function to print a list of the fist n prime numbers  
 '''
-def get_primes(n):
-	primes=[2]
-	current_num=3
+
+# define a function that accepts integer varibale,n 
+def list_first_n_primes_numbers(n):  
+# A list of prime numbers with 2 as the first know prime number
+
+	list_of_primes = [2] 
+	# Assign current_number_being_tested to 3 as the next interger after 2
+	current_number_being_tested = 3 
 	while True:
-		isprime=True
-		for prime in primes:
-			if current_num % prime ==0:
-				isprime=False
-				break
-		if isprime==True:
-			primes.append(current_num)
-		current_num += 1
-		if len(primes)==n:
+		isprime=True # assign a boolean variable to value True
+		for prime in list_of_primes:
+			if current_number_being_tested % prime == 0:
+				isprime = False
+				break				
+		if isprime == True:
+			# Add number to list ofprime numbers if it is a prime number
+			list_of_primes.append(current_number_being_tested)  
+			# increment current_number_being_tested by 1 to go to the next number 
+		current_number_being_tested += 1 
+	
+		if len(list_of_primes) == n:
 			break
-			'''return the nth prime number '''
-	return primes[-1]
-print (get_primes(6))
+			'''return the list of prime number '''
+	return list_of_primes
+# Assign variable m to value of user in put, the nummber of prime numbers to be printed.
+m = int(input("How many prime numbers do you want to print? "))
+print (list_first_n_primes_numbers(m)) # Call function to print a list of first 6 prime numbers
